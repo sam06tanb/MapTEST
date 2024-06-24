4f5b98e35905d538c6079e96598cf9e79bb3d75b
Sam06tanb <78392140+sam06tanb@users.noreply.github.com>
2024-06-24 17:55:15 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> cookies = new TreeMap<>();
+
+        cookies.put("username", "maria");
+        cookies.put("email", "maria@gmail.com");
+        cookies.put("phone", "99711122");
+
+        cookies.remove("email");
+
+        System.out.println("ALL COOKIES: ");
+        for (String key : cookies.keySet()) {
+            System.out.println(key + ": " + cookies.get(key));
+
+           }
+        }
+    }
